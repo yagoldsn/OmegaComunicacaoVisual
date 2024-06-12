0648ccbcd1eaaf141c4829159fc04f1ce4e3ec65
yagoldsn <yagoldsn@gmail.com>
2024-06-11 22:48:49 -0300

    construção navbar

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const app = express();
+const handlebars = require('express-handlebars');
+const path = require('path');//importando opção de usar a pasta public 
 
+//express=handlebars
+const optionsHandlebars = handlebars.create({
+  defaultLayout: "main"
+});
+
+app.engine("handlebars", optionsHandlebars.engine);
+app.set("view engine", "handlebars");
+
+
+//Public
+//Pasta estatica do pro Public
+app.use(express.static(path.join(__dirname, "public")));
 
 
 //ROTAS
 app.get('/', (req, res)=>{
-  res.send('teste"')
+  res.render('home');
 })
 
 //CONFIGURAÇÕES SERVIDOR
