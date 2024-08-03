d2474476411c013264871dfc32db27ab837e7ae2
yagoldsn <yagoldsn@gmail.com>
2024-08-03 11:55:35 -0300

    dropdown menu mobile e desktop

--- public/js/script.js
+++ public/js/script.js
@@ -2,7 +2,7 @@ function menuShow() {
   let menuMobile = document.querySelector(".menu-mobile");
   if (menuMobile.classList.contains("open")) {
     menuMobile.classList.remove("open");
-  } else {
+  } else {   
     menuMobile.classList.add("open");
   }
 }
